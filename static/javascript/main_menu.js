41086beb4df2c9e08d93b2c46cc9a32d942634cc
Stephen <hello@stephenroyce.com>
2023-04-13 22:32:17 +0000

    modified:   prices.db 	modified:   static/javascript/main_menu.js 	modified:   static/style.css 	modified:   templates/base.html

--- static/javascript/main_menu.js
+++ static/javascript/main_menu.js
@@ -9,32 +9,32 @@ function toggleCollapse() {
   window.dispatchEvent(new Event('resize'));
 }
 
-// Window resize
-
-window.addEventListener('resize', () => {
-  if (window.hasOwnProperty('onWindowResize')) {
-    window.onWindowResize();
-  }
-});
-
-function resizeCards() {
-  const cardContainers = document.querySelectorAll('.card-container');
-  const contentWrapper = document.querySelector('.content-wrapper');
-  const isCollapsed = contentWrapper.classList.contains('menu-collapsed');
-  const newWidth = isCollapsed
-    ? `calc((100% - var(--side-menu-collapsed-width) - 32px) / 3)`
-    : `calc((100% - var(--side-menu-width) - 32px) / 3)`;
-
-  cardContainers.forEach((cardContainer) => {
-    cardContainer.style.flex = `0 0 ${newWidth}`;
-    cardContainer.style.maxWidth = newWidth;
+document.addEventListener('DOMContentLoaded', function() {
+  
+  // Window resize
+  window.addEventListener('resize', () => {
+    if (window.hasOwnProperty('onWindowResize')) {
+      window.onWindowResize();
+    }
   });
 
-  // Update the margin-left property of content-wrapper
-  contentWrapper.style.marginLeft = isCollapsed
-    ? `calc(100% - (100% - var(--side-menu-collapsed-width)))`
-    : `calc(100% - (100% - var(--side-menu-width)))`;
-}
+  function resizeCards() {
+    const cardContainers = document.querySelectorAll('.card-container');
+    const contentWrapper = document.querySelector('.content-wrapper');
+    const isCollapsed = contentWrapper.classList.contains('menu-collapsed');
+    const newWidth = isCollapsed
+      ? `calc((100% - var(--side-menu-collapsed-width) - 32px) / 3)`
+      : `calc((100% - var(--side-menu-width) - 32px) / 3)`;
 
-// Call the function to collapse the menu by default
-toggleCollapse(true);
+    cardContainers.forEach((cardContainer) => {
+      cardContainer.style.flex = `0 0 ${newWidth}`;
+      cardContainer.style.maxWidth = newWidth;
+    });
+
+    // Update the margin-left property of content-wrapper
+    contentWrapper.style.marginLeft = isCollapsed
+      ? `calc(100% - (100% - var(--side-menu-collapsed-width)))`
+      : `calc(100% - (100% - var(--side-menu-width)))`;
+  }
+
+});
