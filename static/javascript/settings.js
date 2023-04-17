61ccdf22f809d2958fb81b9210c21f676753b480
Stephen <hello@stephenroyce.com>
2023-04-17 04:44:28 +0000

    Force push

--- static/javascript/settings.js
+++ static/javascript/settings.js
@@ -4,6 +4,8 @@ function saveSettings() {
 
     localStorage.setItem('selectedRegion', regionSelect.value);
     localStorage.setItem('selectedCurrency', currencySelect.value);
+
+    document.getElementById('updateButton').disabled = true;
 }
 
 function loadSettings() {
@@ -19,8 +21,24 @@ function loadSettings() {
     if (selectedCurrency) {
         currencySelect.value = selectedCurrency;
     }
+
+    document.getElementById('updateButton').disabled = true;
+}
+
+function enableUpdateButton() {
+    document.getElementById('updateButton').disabled = false;
 }
 
 // Call the loadSettings function when the page loads
 window.addEventListener('DOMContentLoaded', loadSettings);
 
+// Add the event listener for the 'Update' button
+const updateButton = document.getElementById('updateButton');
+updateButton.addEventListener('click', saveSettings);
+
+// Add event listeners for the select elements to enable the 'Update' button
+const regionSelect = document.getElementById('regionSelect');
+const currencySelect = document.getElementById('currencySelect');
+
+regionSelect.addEventListener('change', enableUpdateButton);
+currencySelect.addEventListener('change', enableUpdateButton);
